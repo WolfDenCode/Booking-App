a7fe834f50bd74130339db98004a071887d267e5
Levente Farkas <levente.farkas.256@gmail.com>
2024-11-30 20:08:52 +0100

    Debug Fixed?

--- src/components/RoomDetails/RoomCard.jsx
+++ src/components/RoomDetails/RoomCard.jsx
@@ -72,7 +72,9 @@ const RoomCard = ({ room, selectedDateRange, onBookingSuccess }) => {
       {selectedDateRange ? (
         <button
           className="book-room-button"
-          onClick={() => handleBooking(room.id, user.id, selectedDateRange)}
+          onClick={() =>
+            handleBooking(room.id, user.user.id, selectedDateRange)
+          }
           disabled={!selectedDateRange.startDate}
         >
           Book Room
